061dda1d5dc102c527f52aa56950df7e1c8d794d
hsyq <lijianzly@gmail.com>
2022-08-06 15:39:47 +0800

    init

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const Mock = require('mockjs')
+
+const app = express()
+
+app.get("/api/users", (req, res) => {
+  const userList = Mock.mock({
+    'userList|10': [{
+      'id|+1': 1,
+      'name': '@cname',
+      'email': '@email'
+    }]
+  })
+
+  res.status(200)
+  res.json(userList)
+})
+
+app.listen(3000, () => {
+  console.log("服务启动: 3000")
+})
